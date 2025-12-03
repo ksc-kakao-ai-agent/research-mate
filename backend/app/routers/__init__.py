459cbbf3f1568bbb9cfdaf0066062f5e09efc4e5
yeooonsu <yeonsu0395@naver.com>
2025-12-03 15:55:27 +0900

    #11 Feat: routers/__init__.py 코드 작성 후 추가

---
+++ backend/app/routers/__init__.py
@@ -0,0 +1,15 @@
+from fastapi import APIRouter
+
+from app.routers.auth_router import router as auth_router
+from app.routers.recommendation_router import router as recommendation_router
+from app.routers.paper_detail_router import router as paper_detail_router
+from app.routers.chatbot_router import router as chatbot_router
+from app.routers.advice_router import router as advice_router
+
+api_router = APIRouter()
+
+api_router.include_router(auth_router, prefix="/api/v1")
+api_router.include_router(recommendation_router, prefix="/api/v1")
+api_router.include_router(paper_detail_router, prefix="/api/v1")
+api_router.include_router(chatbot_router, prefix="/api/v1")
+api_router.include_router(advice_router, prefix="/api/v1")
